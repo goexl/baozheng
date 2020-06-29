939f1ffb2bfc11130ddd3b8fa6cd5daa134a5270
tw-chen <chentw@class100.com>
2020-06-29 14:24:24 +0800

    手机号校验前缀加上+86-

--- validator.go
+++ validator.go
@@ -20,7 +20,7 @@ type Validator struct {
 
 // 检查手机号
 func CheckMobile(mobile string) bool {
-	regular := `^+86-1([3,4,5,6,7,8,9][0-9])\d{8}$`
+	regular := `^[+]86[-]1([3,4,5,6,7,8,9][0-9])\d{8}$`
 	reg := regexp.MustCompile(regular)
 
 	return reg.MatchString(mobile)
