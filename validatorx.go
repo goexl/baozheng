bd37145bfa68396e2d5c27562886a914d238cc93
yangteng@class100.com <Ybjy123>
2021-03-02 11:52:46 +0800

    CLASS330-1619 【Server】添加排课名称所用的校验规则

--- validatorx.go
+++ validatorx.go
@@ -1,6 +1,8 @@
 package validatorx
 
 import (
+	`sync`
+
 	`github.com/go-playground/locales/en`
 	`github.com/go-playground/locales/zh`
 	`github.com/go-playground/universal-translator`
@@ -10,47 +12,49 @@ import (
 )
 
 var (
+	once       sync.Once
 	translator *ut.UniversalTranslator
 	validate   *validator.Validate
+	validatorx *Validatorx
 )
 
 type Validatorx struct {
-	validator *validator.Validate
+	validate *validator.Validate
 }
 
 func (v *Validatorx) Validate(obj interface{}) error {
-	return v.validator.Struct(obj)
+	return v.validate.Struct(obj)
 }
 
-// New 创建
-func New() (validator *Validatorx) {
-	translator = ut.New(en.New(), zh.New())
-	english, _ := translator.GetTranslator("en")
-	if err := enLang.RegisterDefaultTranslations(validate, english); nil != err {
-		panic(err)
-	}
-	chinese, _ := translator.GetTranslator("zh")
-	if err := zhLang.RegisterDefaultTranslations(validate, chinese); nil != err {
-		panic(err)
-	}
+// Init 初始化数据验证器
+func Init() *Validatorx {
+	once.Do(func() {
+		validate = validator.New()
+		translator = ut.New(en.New(), zh.New())
 
-	if err := initValidation(validate); nil != err {
-		panic(err)
-	}
-	if err := initTranslation(validate, chinese); nil != err {
-		panic(err)
-	}
+		english, _ := translator.GetTranslator("en")
+		if err := enLang.RegisterDefaultTranslations(validate, english); nil != err {
+			panic(err)
+		}
+		chinese, _ := translator.GetTranslator("zh")
+		if err := zhLang.RegisterDefaultTranslations(validate, chinese); nil != err {
+			panic(err)
+		}
 
-	validator = &Validatorx{validator: validate}
+		if err := initValidation(validate); nil != err {
+			panic(err)
+		}
+		if err := initTranslation(validate, chinese); nil != err {
+			panic(err)
+		}
 
-	return
+		validatorx = &Validatorx{validate: validate}
+	})
+
+	return validatorx
 }
 
 // Validate 验证
 func Validate(obj interface{}) error {
 	return validate.Struct(obj)
-}
-
-func init() {
-	validate = validator.New()
 }
