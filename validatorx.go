5744c05ea71840a3d70fc60dcb49918b0401cf1b
storezhang <storezhang@gmail.com>
2021-05-25 17:08:51 +0800

    修复盘古循环依赖

--- validatorx.go
+++ validatorx.go
@@ -1,8 +1,6 @@
 package validatorx
 
 import (
-	`sync`
-
 	`github.com/go-playground/locales/en`
 	`github.com/go-playground/locales/zh`
 	`github.com/go-playground/universal-translator`
@@ -14,20 +12,7 @@ import (
 var (
 	validate   *validator.Validate
 	translator *ut.UniversalTranslator
-	once       sync.Once
 )
-
-type Validate struct {
-	validate *validator.Validate
-}
-
-func (v *Validate) Struct(obj interface{}) error {
-	return v.validate.Struct(obj)
-}
-
-func (v *Validate) Var(field interface{}, tag string) error {
-	return v.validate.Var(field, tag)
-}
 
 // Struct 验证结构体
 func Struct(obj interface{}) error {
@@ -39,34 +24,27 @@ func Var(field interface{}, tag string) error {
 	return validate.Var(field, tag)
 }
 
-// New 创建验证器
-func New() *Validate {
-	return &Validate{validate: validate}
-}
-
 // 创建内置验证器
 // 单例设计模式
 func newValidate() (err error) {
-	once.Do(func() {
-		validate = validator.New()
-		translator = ut.New(en.New(), zh.New())
+	validate = validator.New()
+	translator = ut.New(en.New(), zh.New())
 
-		english, _ := translator.GetTranslator("en")
-		if err = enLang.RegisterDefaultTranslations(validate, english); nil != err {
-			return
-		}
-		chinese, _ := translator.GetTranslator("zh")
-		if err = zhLang.RegisterDefaultTranslations(validate, chinese); nil != err {
-			return
-		}
+	english, _ := translator.GetTranslator("en")
+	if err = enLang.RegisterDefaultTranslations(validate, english); nil != err {
+		return
+	}
+	chinese, _ := translator.GetTranslator("zh")
+	if err = zhLang.RegisterDefaultTranslations(validate, chinese); nil != err {
+		return
+	}
 
-		if err = initValidation(validate); nil != err {
-			return
-		}
-		if err = initTranslation(validate, chinese); nil != err {
-			return
-		}
-	})
+	if err = initValidation(validate); nil != err {
+		return
+	}
+	if err = initTranslation(validate, chinese); nil != err {
+		return
+	}
 
 	return
 }
