f682414ce8176c73c72defdaee35cf7e3d0e94c6
storezhang <storezhang@gmail.com>
2021-05-25 13:59:51 +0800

    修复盘古循环依赖

--- go.mod
+++ go.mod
@@ -6,6 +6,5 @@ require (
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
 	github.com/go-playground/validator/v10 v10.6.1
-	github.com/storezhang/gox v1.4.1
-	github.com/storezhang/pangu v1.4.4
+	github.com/storezhang/gox v1.4.10
 )
--- validatorx.go
+++ validatorx.go
@@ -1,6 +1,8 @@
 package validatorx
 
 import (
+	`sync`
+
 	`github.com/go-playground/locales/en`
 	`github.com/go-playground/locales/zh`
 	`github.com/go-playground/universal-translator`
@@ -9,7 +11,11 @@ import (
 	zhLang `github.com/go-playground/validator/v10/translations/zh`
 )
 
-var translator *ut.UniversalTranslator
+var (
+	validate   *validator.Validate
+	translator *ut.UniversalTranslator
+	once       sync.Once
+)
 
 type Validate struct {
 	validate *validator.Validate
@@ -23,31 +29,44 @@ func (v *Validate) Var(field interface{}, tag string) error {
 	return v.validate.Var(field, tag)
 }
 
+// Struct 验证结构体
+func Struct(obj interface{}) error {
+	return validate.Struct(obj)
+}
+
+// Var 验证变量
+func Var(field interface{}, tag string) error {
+	return validate.Var(field, tag)
+}
+
 // New 创建验证器
-func New(validate *validator.Validate) *Validate {
+func New() *Validate {
 	return &Validate{validate: validate}
 }
 
-// NewValidate 创建内置验证器
-func NewValidate() (validate *validator.Validate, err error) {
-	validate = validator.New()
-	translator = ut.New(en.New(), zh.New())
+// 创建内置验证器
+// 单例设计模式
+func newValidate() (err error) {
+	once.Do(func() {
+		validate = validator.New()
+		translator = ut.New(en.New(), zh.New())
 
-	english, _ := translator.GetTranslator("en")
-	if err = enLang.RegisterDefaultTranslations(validate, english); nil != err {
-		return
-	}
-	chinese, _ := translator.GetTranslator("zh")
-	if err = zhLang.RegisterDefaultTranslations(validate, chinese); nil != err {
-		return
-	}
+		english, _ := translator.GetTranslator("en")
+		if err = enLang.RegisterDefaultTranslations(validate, english); nil != err {
+			return
+		}
+		chinese, _ := translator.GetTranslator("zh")
+		if err = zhLang.RegisterDefaultTranslations(validate, chinese); nil != err {
+			return
+		}
 
-	if err = initValidation(validate); nil != err {
-		return
-	}
-	if err = initTranslation(validate, chinese); nil != err {
-		return
-	}
+		if err = initValidation(validate); nil != err {
+			return
+		}
+		if err = initTranslation(validate, chinese); nil != err {
+			return
+		}
+	})
 
 	return
 }
