9ecb5ef454a0acdb10c75c02bfba30adc078418f
yangteng@class100.com <Ybjy123>
2021-08-04 18:35:49 +0800

    修改密码强度检验

--- translation.go
+++ translation.go
@@ -20,5 +20,19 @@ func initTranslation(validate *validator.Validate, chinese ut.Translator) (err e
 		return
 	}
 
+	if err = validate.RegisterTranslation(
+		"password",
+		chinese,
+		func(ut ut.Translator) error {
+			return ut.Add("password", "密码只能由大小写字母、数字，特殊字符组成，且每种类型至少一位", true)
+		},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("password", fe.Field(), fe.Param())
+			return t
+		},
+	); nil != err {
+		return
+	}
+
 	return
 }
